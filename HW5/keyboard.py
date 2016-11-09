f04da5aa6297f2564418ff2fcc29de5e7ceeecb5
Drew Miller <d.m.lespaul@gmail.com>
2016-11-09 12:30:35 -0800

    callback now calls a function that defines the parameters (not set yet)

--- HW5/keyboard.py
+++ HW5/keyboard.py
@@ -10,8 +10,8 @@ block = 5
 class Keyboard(Frame):
     def __init__(self, parent, callback):
         Frame.__init__(self, parent, height=5*block, width=11*block)
-        self.__shift = 0
-        self.__caps = 0
+        self.shift = False
+        self.caps = True
         self.__initwidgets__(callback)
 
     def __initwidgets__(self, callback):
@@ -25,19 +25,35 @@ class Keyboard(Frame):
 
 class KeyButton(Button):
     def __init__(self, parent, t, callback, buttonType):
-        Button.__init__(self, parent, text = t, command= lambda:callback(t))
-
-        __call = callback
+        self.__call = callback
+        Button.__init__(self, parent, text = t, command= lambda:self.__determineOutput__(parent))
 
         if "\n" in t:
             val = t.split("\n")
-            __val1 = val[0]
-            __val2 = val[1]
+            self.__val1 = val[0]
+            self.__val2 = val[1]
         else:
-            __val1 = t
-            __val2 = t
+            self.__val1 = t
+            self.__val2 = t
 
         __type = buttonType
 
-    def __determineOutput__(self):
-        __call(__val1)
+    #check type, parent.shift and parent.caps for the output
+    #set shift to false after using the shifted key
+    def __determineOutput__(self, parent):
+        #for letters
+        if self.__type is "letter":
+            if bool(parent.shift) ^ bool(parent.caps):
+                self.__call(toStr(self.__val2))
+
+        elif self.type is "shift":
+            parent.shift = True
+            self.__call("Shifted " + toStr(parent.Shift))
+
+        elif self.type is "shift":
+            parent.shift = True
+            self.__call("Caps " + toStr(parent.Shift))
+
+        elif self.type is "caps":
+            parent.shift = True
+            self.__call("Shifted " + toStr(parent.Shift))
